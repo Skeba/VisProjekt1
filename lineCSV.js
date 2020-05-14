e6a9203373120aee8d463f784b5192c43523132f
Skeba <seba.lundgren@gmail.com>
2020-05-15 00:01:09 +0300

    Update lineCSV.js

--- lineCSV.js
+++ lineCSV.js
@@ -18,6 +18,30 @@ function drawChart()
         }
         console.log(months);
         console.log(temps);
+
+        // Skapa ritunderlag
+        var width = 800, height = 500;
+        var canvas = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        // vi behöver en skala, för temperatur passar en lineär skala
+        var yScale = d3.scaleLinear()
+            .domain([d3.min(temps), d3.max(temps)]) // vilka värden ska konveretas till pixelvärden
+            .range([0,height]);
+
+
+        // Generera D strängen för path
+        var dString = d3.line()
+            .x(function(data) { return data.Month })
+            .y(function(data) { return data.Temp });
+
+        // Rita linjen
+        canvas.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "blue")
+            .attr("d", dString(data));
     });
 
     // d3 ES6 V5 SYNTAx
