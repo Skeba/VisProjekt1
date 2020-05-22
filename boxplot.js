6c85b048f9cea639a185ed1b7aa6c221d7f94c4c
Sebastian Lundgren <lundgres@arcada.fi>
2020-05-22 10:49:01 +0300

    Update boxplot.js
    Lektion 7

--- boxplot.js
+++ boxplot.js
@@ -3,9 +3,11 @@ function drawBoxplot()
     // Läs in extern JSON data (d3.v5)
     d3.json("boxplot.json").then( function(jsonData) {
 
-        var width = 600, height = 300, margin = 30;
+        var width = 600, height = 150, margin = 30;
         var chartWidth = width - (margin*2);
         var chartHeight = height - (margin*2);
+        var boxHeight = 50, boxPosition = chartHeight-boxHeight;
+        var lineY = boxPosition + boxHeight / 2;
 
         // Spara json i JS arrays
         // Ladda in datan
@@ -43,12 +45,48 @@ function drawBoxplot()
         // Skapa en grupp som inte täcker hela ritunderlaget
         var chartGroup = canvas.append("g").attr("transform", "translate(0,0)");
 
-        chartGroup.append("rect")
-            .attr("width", function(data) { return xScale(uq) - xScale(lq) } )
-            .attr("height", 50)
-            .attr("x", xScale(lq) )
-            .attr("y", 0);
+        // Rita en linje från Q3 till maxvärdet
+        chartGroup.append("line")
+            .attr("width", 5)
+            .attr("stroke", "black")
+            .attr("x1",xScale(min))
+            .attr("x2",xScale(max))
+            .attr("y1", lineY)
+            .attr("y2", lineY);
 
+        chartGroup.append("rect")
+            .attr("width", xScale(uq) - xScale(lq) )
+            .attr("height", boxHeight)
+            .attr("fill", "green")
+            .attr("stroke", "black")
+            .attr("x", xScale(lq) )
+            .attr("y", boxPosition);
+
+
+        // Rita en linje från Qmin till Q2
+        chartGroup.append("line")
+            .attr("width", 5)
+            .attr("stroke", "black")
+            .attr("x1",xScale(min))
+            .attr("x2",xScale(min))
+            .attr("y1", boxPosition + 10)
+            .attr("y2", boxPosition+boxHeight - 10);
+
+        chartGroup.append("line")
+            .attr("width", 5)
+            .attr("stroke", "black")
+            .attr("x1",xScale(max))
+            .attr("x2",xScale(max))
+            .attr("y1", boxPosition + 10)
+            .attr("y2", boxPosition+boxHeight - 10);
+
+        chartGroup.append("line")
+            .attr("width", 5)
+            .attr("stroke", "black")
+            .attr("x1",xScale(median))
+            .attr("x2",xScale(median))
+            .attr("y1", boxPosition)
+            .attr("y2", boxPosition+boxHeight);
         // Rita ut axeln
         chartGroup.append("g").call(xAxis);
     });
